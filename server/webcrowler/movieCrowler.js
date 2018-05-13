a3237fcd19b67689a1fcfed2df2c92736a4fa13b
xb6022801 <binxuqz@gmail.com>
2018-05-13 12:02:15 +0200

    commit

--- server/webcrowler/movieCrowler.js
+++ server/webcrowler/movieCrowler.js
@@ -9,9 +9,7 @@ var cheerio = require('cheerio'),
 
 var ep = new eventproxy()
     entryUrl = 'https://www.dy2018.com/html/gndy/dyzz/index.html',
-    pageUrls = [],
-    titles_temp = []
-    
+    pageUrls = []    
 
 function start() {
   var curCount = 0
@@ -27,38 +25,49 @@ function start() {
               select = $('select option')
          
           res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
-          for (var i = 0; i < select.length; i++) { 
+          var requestLength = Math.min(select.length, 10) // 只取前10页的内容
+
+          for (var i = 0; i < requestLength; i++) { 
             var href = select.eq(i).attr('value')
             pageUrls.push('https://www.dy2018.com' + href)
           }
 
-          //step 2: use async.mapLimit(coll, limit, iteratee, callbackopt) control concurrent
+          //use async.mapLimit(coll, limit, iteratee, callbackopt) control concurrent
           async.mapLimit(pageUrls, 5, function(url, callback) {
-            crowler(url, callback)
+            debounceFn(crowler, url, callback)
           }, function(err, results) {
             if (err) {
               console.log(err)
-            } else {
-              console.log(results)
-              fs.writeFile('result.json', JSON.stringify(titles_temp, 0, 4),
-              'utf-8', (err, data) => {
-                if (err) {
-                  console.log(err)
-                }
-               }
-              )
             }
           }) // end async
-
         } // end superagent
+      }) // end end
+
+      // step 1: got each entry page, then crowler every movie per page
+      ep.after('got_links', 200, function(links) {
+        curCount = 0
+        async.mapLimit(links, 5, function(item, cb) {
+          debounceFn(analyseMovie, item, cb)
+        }, function(err, result) {
+          if (err) {
+            console.log(err)
+          }
+        })
       })
+
+      // step 2: conclure analyzed data, send to client
+      ep.after('movie_analyzed', 200, function(items) {
+        console.log(items)
+      })
+    
   } // end onRequest
 
+  /**
+   * @param {*} pageUrl 
+   * @param {*} callback 
+   * @description get every movie links per page
+   */
   function crowler(pageUrl, callback) {
-    var delay = parseInt((Math.random() * 30000000) % 1000, 10)
-    curCount++
-    console.log('现在的并发数是', curCount, '，正在抓取的是', pageUrl, '，耗时' + delay + '毫秒')
-
     superagent.get(pageUrl)
       .charset('gbk')
       .end(function(err, response) {
@@ -71,18 +80,85 @@ function start() {
           for (var i = 0; i < select.length; i++) { 
             var title = select.eq(i).attr('title'),
                 link = 'https://www.dy2018.com' + select.eq(i).attr('href')
-            titles_temp.push({ title, link })
+            ep.emit('got_links', { title, link })
           }
         }
       })    
+  }
 
-      setTimeout(function() {
-        curCount--;
-        callback(null, 'Call back content');
-      }, delay)
+  /**
+   * @param {*} item 
+   * @param {*} cb 
+   * @description analyse movie
+   */
+  function analyseMovie(item, cb) {
+    superagent.get(item.link)
+      .charset('gbk')
+      .end(function(err, response) {
+        if (err) {
+          console.log(err)
+        } else {
+          parseMovie(response.text)
+        }
+      })  
+  }
+  
+  /**
+   * @param {*} args 
+   * @description debounce execution of every http request
+   */
+  function debounceFn(...args) {
+    var targetFn = args.shift(),
+        cb = args.pop(),
+        delay = parseInt((Math.random() * 30000000) % 1000, 10)
+
+    if (!targetFn instanceof Function) {
+      throw new Error('no function argument')
+    }
+
+    curCount++
+    console.log('analyseMovie 现在的并发数是', curCount, ', 耗时' + delay + '毫秒')
+    targetFn.apply(null, args)
+
+    setTimeout(function() {
+      curCount--;
+      cb(null, 'Call back content');
+    }, delay)  
+  }
+
+  /**
+   * @param {*} rawText 
+   * @description parse html of movie page
+   */
+  function parseMovie(rawText) {
+    var $ = cheerio.load(rawText),
+        metaDatas = $('.co_content8 .position span'),
+          title = $('.title_all h1').text(),
+          rank = metaDatas.eq(0).find('.rank').text(),
+          date = metaDatas.eq(2).text(),
+          types = metaDatas.eq(1).find('a'),
+          typeArray = []        
+
+    for (var i = 0; i < types.length; i++) {
+      typeArray.push(types.eq(i).text())
+    }
+    ep.emit('movie_analyzed', {
+      title,
+      rank,
+      date,
+      type: typeArray.join(' / ')
+    })
   }
 
   http.createServer(onRequest).listen(8000)
 }
 
 exports.start = start
+
+// fs.writeFile('result.json', JSON.stringify(titles_temp, 0, 4),
+// 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err)
+//   }
+//  }
+// ) // end write file
